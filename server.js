11ca98d6555a87650d08739a0ff9d7c00c3d1f76
SergSCys <zkglbq7o4yd9@opayq.net>
2022-03-16 13:49:49 +0100

    fixed exphbs - pushing to merge

--- server.js
+++ server.js
@@ -21,13 +21,15 @@ app.get('/', (req, res) => {
 
 app.set('views', path.join(__dirname, '/views/'))
 
-app.engine('hbs', exphbs({
+app.engine('hbs', exphbs.engine({
     handlebars: allowInsecurePrototypeAccess(handlebars),
-    extname: "hbs",
-    defaultLayout: "MainLayout",
-    layoutsDir: __dirname + "/views/layouts/",
+    extname: 'hbs',
+    defaultLayout: 'MainLayout',
+    layoutsDir: __dirname + '/views/layouts/',
     })
 );
+
+app.set("view engine", "hbs");
 
 app.listen(8000, () => {
     console.log('Serveur opérationnel');
