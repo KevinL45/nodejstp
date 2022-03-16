5c9313837143a894d244776b58e719b96610aab2
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2022-03-16 09:52:29 +0100

    tp

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const body = require('body-parser')
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
     res.send('Serveur opérationnel !');
 });
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
 app.listen(8000, () => {
     console.log('Serveur opérationnel');
