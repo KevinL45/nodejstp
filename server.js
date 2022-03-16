518dd54355946cb9eddfe42b3729e86502807cea
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2022-03-16 09:54:35 +0100

    tp

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const body = require('body-parser')
+const bodyParser = require('body-parser')
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
