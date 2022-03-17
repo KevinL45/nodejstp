790e666f40e7e216940552d3e81c73cb96dc6f01
Kevin Lokoka <68908952+KevinL45@users.noreply.github.com>
2022-03-17 16:18:07 +0100

    qrcode

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-
 const path = require('path');
 const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
-
-const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const expressLayouts = require('express-ejs-layouts')
+const ContactService = require('./services/ContactService')
+const expressLayouts = require('express-ejs-layouts');
+const qrCode = require('qrcode');
+const { json } = require('express/lib/response');
+const VCardGenerator = require('./js/generator/VCardGenerator');
 
+const app = express();
 
 
 app.use(expressLayouts)
 app.set('views','./views')
 app.set('view engine','ejs')
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
 mongoose.connect('mongodb+srv://nodejstp:3qvBumFuC212Zmjh@cluster0.sub0n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser : true , useUnifiedTopology : true},
 () =>{
@@ -23,8 +30,6 @@ mongoose.connect('mongodb+srv://nodejstp:3qvBumFuC212Zmjh@cluster0.sub0n.mongodb
 
 //Routes
 app.get('/', (req, res) => {
-
-    let ContactService = require('./services/ContactService')
     let contactService = new ContactService()
     let contacts = contactService.getAll()
     res.render('home/home', {contacts : contacts})
@@ -42,10 +47,17 @@ app.get('/contact/modifier', (req, res) => {
 });
 
 app.get('/contact/detail/:id', (req, res) => {
-    let ContactService = require('./services/ContactService')
+
     let contactService = new ContactService()
+    let vcard = new VCardGenerator()
     let contact = contactService.getContact(req.params.id)
-    res.render('contact/details', {contact: contact})
+      
+    qrCode.toDataURL(vcard.generate(contact),function (err,url){
+        res.render('contact/details', {contact: contact, qrCode : url})
+    })
+
+
+
 });
 
 app.get('/utilisateur/connexion', (req, res) => {
@@ -54,11 +66,6 @@ app.get('/utilisateur/connexion', (req, res) => {
 app.get('/utilisateur/inscription', (req, res) => {
     res.render('user/signup')
 });
-
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({
-        extended: true
-    }));
 
 app.listen(8000, () => {
 
